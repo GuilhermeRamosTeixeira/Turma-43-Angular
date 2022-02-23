1952be5a6a12256ab13e4483ec6009d88fca39a0
GuilhermeRamosTeixeira <guilherme.98ramos@hotmail.com>
2022-02-23 14:20:22 -0300

    component inicio sem consumo

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -24,6 +25,14 @@ cadastrar(usuario:Usuario): Observable<Usuario>{
 
 }
 
+logado(){
+  let ok: boolean =false
+
+  if(environment.token != ""){
+    ok = true
+  }
+  return ok
+}
 
 }
 
